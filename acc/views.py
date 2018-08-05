fe5b0be676b1a8eed3b34915828b15658881955a
Gamalovic <omar.gamalfciscu@gmail.com>
2018-08-05 22:37:45 +0200

    setting up massanger configuration

--- acc/views.py
+++ acc/views.py
@@ -76,75 +76,75 @@ class Msg(APIView):
 
 
         
-    def get(self, request, *args, **kwargs):
-        if self.request.GET['hub.verify_token'] == 'floki':
-            print("success !")
-            return HttpResponse(self.request.GET['hub.challenge'])
-        else:
-            return HttpResponse('Error, invalid token')
+    # def get(self, request, *args, **kwargs):
+    #     if self.request.GET['hub.verify_token'] == 'floki':
+    #         print("success !")
+    #         return HttpResponse(self.request.GET['hub.challenge'])
+    #     else:
+    #         return HttpResponse('Error, invalid token')
     
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return generic.View.dispatch(self, request, *args, **kwargs)
+    # @method_decorator(csrf_exempt)
+    # def dispatch(self, request, *args, **kwargs):
+    #     return generic.View.dispatch(self, request, *args, **kwargs)
 
-    # Post function to handle Facebook messages
-    def post(self, request, *args, **kwargs):
-        # Converts the text payload into a python dictionary
-        incoming_message = json.loads(self.request.body.decode('utf-8'))
-        print("#######")
-        print(incoming_message)
-        # Facebook recommends going through every entry since they might send
-        # multiple messages in a single call during high load
-        i=0
-        for entry in incoming_message['entry']:
-            for message in entry['messaging']:
-                # Check to make sure the received call is a message call
-                # This might be delivery, optin, postback for other events 
-                if 'message' in message:
-                    # Print the message to the terminal
-                    i+=1
-                    result=dialogAiml(message['message']['text'])
-                    print("#######")
-                    print(message['message']['text'])
-                    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAO3eCzKNI4BAPk5d96ZArJ6QmY8Pv8pKwhcDckbnWzpaT5rHbsIr8H8MPeZCPJ1KaxZCwBtyjAgVpiB66qZBQ1ydrHL3irumlZA5uIFT8Vi3ZB24kfT8cAfOagjOjCQyGves6azjSf5JDohTkCYeTx5zzOhdDSlNKrZC5trYDiWxFbhryYZBFAacqN4ozhDRFYZD' 
-                    response_msg = json.dumps({"recipient":{"id":message['recipient']['id']}, "message":{"text":result}})
-                    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
-                    print("#######")
-                    print(message)
+    # # Post function to handle Facebook messages
+    # def post(self, request, *args, **kwargs):
+    #     # Converts the text payload into a python dictionary
+    #     incoming_message = json.loads(self.request.body.decode('utf-8'))
+    #     print("#######")
+    #     print(incoming_message)
+    #     # Facebook recommends going through every entry since they might send
+    #     # multiple messages in a single call during high load
+    #     i=0
+    #     for entry in incoming_message['entry']:
+    #         for message in entry['messaging']:
+    #             # Check to make sure the received call is a message call
+    #             # This might be delivery, optin, postback for other events 
+    #             if 'message' in message:
+    #                 # Print the message to the terminal
+    #                 i+=1
+    #                 result=dialogAiml(message['message']['text'])
+    #                 print("#######")
+    #                 print(message['message']['text'])
+    #                 post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAO3eCzKNI4BAPk5d96ZArJ6QmY8Pv8pKwhcDckbnWzpaT5rHbsIr8H8MPeZCPJ1KaxZCwBtyjAgVpiB66qZBQ1ydrHL3irumlZA5uIFT8Vi3ZB24kfT8cAfOagjOjCQyGves6azjSf5JDohTkCYeTx5zzOhdDSlNKrZC5trYDiWxFbhryYZBFAacqN4ozhDRFYZD' 
+    #                 response_msg = json.dumps({"recipient":{"id":message['recipient']['id']}, "message":{"text":result}})
+    #                 status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    #                 print("#######")
+    #                 print(message)
                       
-                    print(result) 
+    #                 print(result) 
                      
-        return HttpResponse()
+    #     return HttpResponse()
 
 
     ## AIML
 
-    # def post(self, request):
-    #     message  = request.data['name']
+    def post(self, request):
+        message  = request.data['name']
 
-    #     filtered_Message=Filtering(message)
-    #     if(filtered_Message =="schedule"):
-    #         queryset = schedule.objects.all()
-    #         r=serialize('json', queryset)
-    #         jsonData=json.loads(r)
-    #         response={"data":jsonData,"source":"db"}
-    #         return Response(json.dumps(response))
+        filtered_Message=Filtering(message)
+        if(filtered_Message =="schedule"):
+            queryset = schedule.objects.all()
+            r=serialize('json', queryset)
+            jsonData=json.loads(r)
+            response={"data":jsonData,"source":"db"}
+            return Response(json.dumps(response))
             
         
-    #     elif(filtered_Message=="event"):
-    #         queryset = Event.objects.all()
-    #         r = serialize('json', queryset)
-    #         jsonData = json.loads(r)
-    #         response={"data":jsonData,"source":"event"}
-    #         return Response(json.dumps(response))
+        elif(filtered_Message=="event"):
+            queryset = Event.objects.all()
+            r = serialize('json', queryset)
+            jsonData = json.loads(r)
+            response={"data":jsonData,"source":"event"}
+            return Response(json.dumps(response))
             
 
-    #     else:
-    #         if request.method=='POST':
+        else:
+            if request.method=='POST':
             
-    #             res=dialogAiml(message)
-    #             response={"data":res,"source":"AI"}
-    #             return Response(json.dumps(response))
+                res=dialogAiml(message)
+                response={"data":res,"source":"AI"}
+                return Response(json.dumps(response))
         
 ################################### sign up API #######################################
 
