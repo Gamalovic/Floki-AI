b29394ecd343065540c443219e59563a0937d3d7
Gamalovic <omar.gamalfciscu@gmail.com>
2018-08-05 19:05:31 +0200

    setting up massanger configuration

--- acc/views.py
+++ acc/views.py
@@ -98,7 +98,7 @@ class Msg(APIView):
                     # Print the message to the terminal
                     result=dialogAiml(message['message']['text'])
                     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAO3eCzKNI4BAPk5d96ZArJ6QmY8Pv8pKwhcDckbnWzpaT5rHbsIr8H8MPeZCPJ1KaxZCwBtyjAgVpiB66qZBQ1ydrHL3irumlZA5uIFT8Vi3ZB24kfT8cAfOagjOjCQyGves6azjSf5JDohTkCYeTx5zzOhdDSlNKrZC5trYDiWxFbhryYZBFAacqN4ozhDRFYZD' 
-                    response_msg = json.dumps({"recipient":{"id":message['sender']['id']}, "message":{"text":message['message']['text']}})
+                    response_msg = json.dumps({"recipient":{"id":message['sender']['id']}, "message":{"text":result}})
                     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
                     pprint(message)     
         return HttpResponse()
