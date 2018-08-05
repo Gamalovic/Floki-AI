819e200ee0469413b8979168dc644c0a30b926cc
Gamalovic <omar.gamalfciscu@gmail.com>
2018-08-05 19:00:33 +0200

    setting up massanger configuration

--- acc/urls.py
+++ acc/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from acc import views
 
-from acc.views import FlokiBotView
 ############################api#############################
 #from rest_framework.urlpatterns import format_suffix_patterns
 #############################################################
@@ -25,7 +24,7 @@ urlpatterns = [
     path('apilogin/', views.userLoginView.as_view()),
     path('msg/', views.Msg.as_view()),
     path('data/', include(router.urls)),
-    path('374ee80922e1c0e51a0c45e6bf097c1f6cfa6990945ed6592a/', FlokiBotView.as_view()),
+    
    
 
 
--- acc/views.py
+++ acc/views.py
@@ -42,19 +42,17 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from django.views import generic
 from django.http.response import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
 #################################################################################
 ############## Annoying imports #################################################
 #################################################################################
 
 
 
-class FlokiBotView(generic.View):
-    def get(self, request, *args, **kwargs):
-        if self.request.GET['hub.verify_token'] == 'floki':
-            print("success !")
-            return HttpResponse(self.request.GET['hub.challenge'])
-        else:
-            return HttpResponse('Error, invalid token')
+
+    
 
 
 
@@ -75,7 +73,35 @@ class Msg(APIView):
     ##        print(res)
     ##        return Response(res)
         #return Response(serializer.errors)
+    def get(self, request, *args, **kwargs):
+        if self.request.GET['hub.verify_token'] == 'floki':
+            print("success !")
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
+            return HttpResponse('Error, invalid token')
+    
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return generic.View.dispatch(self, request, *args, **kwargs)
 
+    # Post function to handle Facebook messages
+    def post(self, request, *args, **kwargs):
+        # Converts the text payload into a python dictionary
+        incoming_message = json.loads(self.request.body.decode('utf-8'))
+        # Facebook recommends going through every entry since they might send
+        # multiple messages in a single call during high load
+        for entry in incoming_message['entry']:
+            for message in entry['messaging']:
+                # Check to make sure the received call is a message call
+                # This might be delivery, optin, postback for other events 
+                if 'message' in message:
+                    # Print the message to the terminal
+                    result=dialogAiml(message['message']['text'])
+                    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAO3eCzKNI4BAPk5d96ZArJ6QmY8Pv8pKwhcDckbnWzpaT5rHbsIr8H8MPeZCPJ1KaxZCwBtyjAgVpiB66qZBQ1ydrHL3irumlZA5uIFT8Vi3ZB24kfT8cAfOagjOjCQyGves6azjSf5JDohTkCYeTx5zzOhdDSlNKrZC5trYDiWxFbhryYZBFAacqN4ozhDRFYZD' 
+                    response_msg = json.dumps({"recipient":{"id":message['sender']['id']}, "message":{"text":message['message']['text']}})
+                    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+                    pprint(message)     
+        return HttpResponse()
 
 
     ## AIML
